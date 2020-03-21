7f154c42a8c9586763d2234fbb57b105f95603ab
Niggarilomon <losgamers123456@gmail.com>
2020-03-21 09:13:35 -0300

    socket bug

--- src/index.js
+++ src/index.js
@@ -12,7 +12,6 @@ const app = express();
 
 const port = process.env.PORT || '3000';
 const server = app.listen(port, () => console.log('Open Server'));
-const io = require('socket.io').listen(server);
 
 // ======================== Configurações Gerais ======================= //
 
